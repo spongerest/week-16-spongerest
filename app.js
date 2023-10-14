962dc2a09016557d8c23c56175d61326e0cf21f4
Restu Windri P <114996009+spongerest@users.noreply.github.com>
2023-10-14 07:11:59 +0700

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ require("dotenv").config()
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
